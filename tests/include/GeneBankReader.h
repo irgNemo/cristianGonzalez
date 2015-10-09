a1033b23f47408bac9614ba1b10bf82ff8717563
CristianDavid <cristian_david_g@live.com>
2015-10-08 23:45:18 -0500

    Clase para leer archivos .gb

---
+++ tests/include/GeneBankReader.h
@@ -0,0 +1,18 @@
+/*
+ * Author: Cristian David Gonzalez Gonzalez.
+ * Institution: UdG-CUCEI.
+ * Created: October 6th 2015.
+ */
+ 
+#ifndef GENE_BANK_READER_H_INCLUDED
+#define GENE_BANK_READER_H_INCLUDED
+
+#include "SequenceReader.h"
+#include "SequenceInfo.h"
+
+class GeneBankReader : public SequenceReader {
+   SequenceInfo *readSequence(const char *fileName) override;
+   bool supportsExtention(const char *ext) override;
+};
+
+#endif // GENE_BANK_READER_H_INCLUDED
