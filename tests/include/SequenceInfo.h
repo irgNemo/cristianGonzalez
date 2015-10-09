765b8e2ec74c4532e0e2eb5de289b8336beabb2d
CristianDavid <cristian_david_g@live.com>
2015-10-08 23:35:48 -0500

    Clase base para almacenar información de secuencias procedente de un formato de archivo determinado, como .fasta o .gb

---
+++ tests/include/SequenceInfo.h
@@ -0,0 +1,17 @@
+/*
+ * Author: Cristian David Gonzalez Gonzalez.
+ */
+
+#ifndef SEQUENCE_INFO_INCLUDED
+#define SEQUENCE_INFO_INCLUDED
+
+#include <string>
+
+class SequenceInfo {
+   virtual ~SequenceInfo();
+   virtual void setSequenceData(const std::string &sequenceData) = 0;
+   virtual const std::string &getSequenceData() const = 0;
+   virtual const std::string &getFormatName() const = 0;
+};
+
+#endif // SEQUENCE_INFO_INCLUDED
