14302ad6cc8df5ede81930f2fc28ad4e063aa1c8
CristianDavid <cristian_david_g@live.com>
2015-10-09 01:19:31 -0500

    Cambio menor en el archivo

--- tests/include/SequenceInfo.h
+++ tests/include/SequenceInfo.h
@@ -12,7 +12,7 @@ class SequenceInfo {
    virtual ~SequenceInfo() = default;
    virtual void setSequenceData(const std::string &sequenceData) = 0;
    virtual const std::string &getSequenceData() const = 0;
-   virtual const std::string &getFormatName() const = 0;
+   virtual const std::string &getFormatName()   const = 0;
 };
 
 #endif // SEQUENCE_INFO_INCLUDED
