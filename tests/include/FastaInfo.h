32022c27133f215d252f77418fa283ebfdd4090f
CristianDavid <cristian_david_g@live.com>
2015-10-08 23:39:04 -0500

    Clase para guradar la información de un archivo .fasta

---
+++ tests/include/FastaInfo.h
@@ -0,0 +1,27 @@
+/*
+ * Author: Cristian David Gonzalez Gonzalez.
+ */
+
+#ifndef FASTA_INFO_INCLUDED
+#define FASTA_INFO_INCLUDED
+
+#include <string>
+#include "SequenceInfo.h"
+
+class FastaInfo : public SequenceInfo {
+ public:
+   FastaInfo(const std::string &description, const std::string &sequenceData);
+   FastaInfo() = default;
+   void  setSequenceData(const std::string &sequenceData) override;
+   void  setDescription(const string &description);
+   const std::string &getSequenceData() const override;
+   const std::string &getDescription()  const;
+   const std::string &getFormatName()   const override;
+ private:
+   std::string description;
+   std::string sequenceData;
+};
+
+
+
+#endif // FASTA_INFO_INCLUDED
